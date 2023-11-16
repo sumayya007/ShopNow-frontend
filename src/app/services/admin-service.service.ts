f2283cb1d91d43324bd0594e71c68856921ddac4
sumayya <sumayya.kareem6@gmail.com>
2023-11-16 08:52:38 +0530

    Update admin-service.service.ts

--- src/app/services/admin-service.service.ts
+++ src/app/services/admin-service.service.ts
@@ -14,39 +14,39 @@ export class AdminServiceService {
    ;
       console.log('job update')
     
-      return this.http.put("https://shopnow-t9zl.onrender.com/admin/updateadmin/"+id,{"user":user}).subscribe(data=>{console.log("data is"+data)});
+      return this.http.put("https://shopnowapi-ydrz.onrender.com/admin/updateadmin/"+id,{"user":user}).subscribe(data=>{console.log("data is"+data)});
     }
   
     adminEditUser(id:any,user:any){
       console.log("hey user is ",user)
       const newuser=user[0];
       console.log("hey user is ",newuser)
-      return this.http.put("https://shopnow-t9zl.onrender.com/admin/adminupdateuser/"+id,{"user":newuser}).subscribe(data=>{console.log("data is"+data)});
+      return this.http.put("https://shopnowapi-ydrz.onrender.com/admin/adminupdateuser/"+id,{"user":newuser}).subscribe(data=>{console.log("data is"+data)});
     }
     adminEditProduct(id:any,product:any){
       console.log("hey user is ",product)
       const newproduct=product[0];
-      return this.http.put("https://shopnow-t9zl.onrender.com/admin/adminupdateproduct/"+id,{"product":newproduct}).subscribe(data=>{console.log("data is"+data)});
+      return this.http.put("https://shopnowapi-ydrz.onrender.com/admin/adminupdateproduct/"+id,{"product":newproduct}).subscribe(data=>{console.log("data is"+data)});
     }
     adminEditCategory(id:any,category:any){
       console.log("hey user is ",category)
       const newcategory=category[0];
-      return this.http.put("https://shopnow-t9zl.onrender.com/admin/adminupdatecategory/"+id,{"category":newcategory}).subscribe(data=>{console.log("data is"+data)});
+      return this.http.put("https://shopnowapi-ydrz.onrender.com/admin/adminupdatecategory/"+id,{"category":newcategory}).subscribe(data=>{console.log("data is"+data)});
     }
     
 
     removeUser(id:any){
-      return this.http.delete("https://shopnow-t9zl.onrender.com/admin/removeUser/"+id);
+      return this.http.delete("https://shopnowapi-ydrz.onrender.com/admin/removeUser/"+id);
     }
     removeCategory(id:any){
-      return this.http.delete("https://shopnow-t9zl.onrender.com/admin/removeCategory/"+id);
+      return this.http.delete("https://shopnowapi-ydrz.onrender.com/admin/removeCategory/"+id);
     }
   getCategoryList() {
-    return this.http.get<any>("https://shopnow-t9zl.onrender.com/admin/getCategoriesList");
+    return this.http.get<any>("https://shopnowapi-ydrz.onrender.com/admin/getCategoriesList");
   }
  
   getProductsList() {
-    return this.http.get<any>("https://shopnow-t9zl.onrender.com/admin/getProductsList");
+    return this.http.get<any>("https://shopnowapi-ydrz.onrender.com/admin/getProductsList");
   }
 
  
@@ -54,35 +54,35 @@ export class AdminServiceService {
  
   getUsersList() {
     console.log("inside getemployers")
-    return this.http.get<any>("https://shopnow-t9zl.onrender.com/user/getUsers");
+    return this.http.get<any>("https://shopnowapi-ydrz.onrender.com/user/getUsers");
   }
   
   addProduct(product:any){
-    return this.http.post<any>("https://shopnow-t9zl.onrender.com/admin/addProduct",{"product":product}).subscribe(data=>{console.log(data)});
+    return this.http.post<any>("https://shopnowapi-ydrz.onrender.com/admin/addProduct",{"product":product}).subscribe(data=>{console.log(data)});
   }
   addToCart(product:any) {
-    return this.http.post<any>("https://shopnow-t9zl.onrender.com/admin/addToCart",{"product":product}).subscribe(data=>{console.log(data)});
+    return this.http.post<any>("https://shopnowapi-ydrz.onrender.com/admin/addToCart",{"product":product}).subscribe(data=>{console.log(data)});
   }
  
   removeProduct(id: any) {
-    return this.http.delete("https://shopnow-t9zl.onrender.com/admin/removeProduct/"+id);
+    return this.http.delete("https://shopnowapi-ydrz.onrender.com/admin/removeProduct/"+id);
   }
   addCategory(category:any){
-    return this.http.post<any>("https://shopnow-t9zl.onrender.com/admin/addCategory",{"category":category}).subscribe(data=>{console.log(data)});
+    return this.http.post<any>("https://shopnowapi-ydrz.onrender.com/admin/addCategory",{"category":category}).subscribe(data=>{console.log(data)});
   }
   getAdminById(id:any){
-    return this.http.get<any>("https://shopnow-t9zl.onrender.com/admin/getadminbyid/"+id);
+    return this.http.get<any>("https://shopnowapi-ydrz.onrender.com/admin/getadminbyid/"+id);
   }
   updateAdmin(admin:any) {
-    return this.http.put("https://shopnow-t9zl.onrender.com/admin/update",admin).subscribe(data=>{console.log("data is"+data)});
+    return this.http.put("https://shopnowapi-ydrz.onrender.com/admin/update",admin).subscribe(data=>{console.log("data is"+data)});
   }
   getProductById(id:any){
-    return this.http.get<any>("https://shopnow-t9zl.onrender.com/admin/getproductbyid/"+id);
+    return this.http.get<any>("https://shopnowapi-ydrz.onrender.com/admin/getproductbyid/"+id);
   }
   getUserById(id:any){
-    return this.http.get<any>("https://shopnow-t9zl.onrender.com/admin/getuserbyid/"+id);
+    return this.http.get<any>("https://shopnowapi-ydrz.onrender.com/admin/getuserbyid/"+id);
   }
   getCategoryById(id:any){
-    return this.http.get<any>("https://shopnow-t9zl.onrender.com/admin/getcategorybyid/"+id);
+    return this.http.get<any>("https://shopnowapi-ydrz.onrender.com/admin/getcategorybyid/"+id);
   }
 }
